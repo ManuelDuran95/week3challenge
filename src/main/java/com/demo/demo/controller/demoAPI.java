6e249cac6726b130e1c62816107d1bd85b8808a3
manuelduran <mata.duran@gmail.com>
2024-10-14 19:16:11 +0000

    update controller v1

--- src/main/java/com/demo/demo/controller/demoAPI.java
+++ src/main/java/com/demo/demo/controller/demoAPI.java
@@ -3,11 +3,18 @@ package com.demo.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController("/api")
 public class demoAPI {
+    @Value("${test.variable}")
+    private String demoVar;
+
     @GetMapping("/greet/{name}")
     public String greet(@PathVariable String name){
-        return "Hello "+name + "APIv3";
+      
+
+        System.out.println("Variable from properties" + demoVar); 
+        return "Hello "+name + "APIv3" +demoVar;
     }
 }
