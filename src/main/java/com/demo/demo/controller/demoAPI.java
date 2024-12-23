55641abf2b293a08febaec5000e8305c3e7f2c1f
Manuel Durán <mata.duran@gmail.com>
2024-12-23 16:37:05 -0600

    Update demoAPI.java

--- src/main/java/com/demo/demo/controller/demoAPI.java
+++ src/main/java/com/demo/demo/controller/demoAPI.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @RestController("/api")
 public class demoAPI {
